19446c6b24a44855c9e2403278262111844bbc9f
HyunSeok0314 <64994672+HyunSeok0314@users.noreply.github.com>
2020-05-13 22:26:41 +0900

    Update sungsusbBOT.py

--- sungsusbBOT.py
+++ sungsusbBOT.py
@@ -21,7 +21,7 @@ async def on_message(message):
 
 
     if message.content.startswith("+도움말"):
-        await message.channel.send(message.author.mention+"```css\n성은봇 사용법\n#1. '성은아'를 붙어 봇을 사용한다\n#2. '성은아 안녕'등 명령어들이 있다(점점 추가 함)\n#3. '+문의'로 문의\n'+관리자 [정보/소환]으로 관리자의 정보를 확인할 수 있다'\n...```")
+        await message.channel.send(message.author.mention+"```css\n성은봇 사용법\n#1. '성은아'를 붙어 봇을 사용한다\n#2. '성은아 안녕'등 명령어들이 있다(점점 추가 함)\n#3. '+문의'로 문의\n'#4. +관리자 [정보/소환]으로 관리자의 정보를 확인할 수 있다'\n...```")
         
     elif message.content.startswith("+문의"):
         atr = message.guild.get_member(653172394499768322)
@@ -30,8 +30,8 @@ async def on_message(message):
         if msg == "":
             await message.channel.send(message.author.mention + "```css\n문의 내용을 적어주세요.(+문의 [내용])\n```")
         else:
-            await atr1.send(message.author.mention + " : " + msg)
-            await atr.send(message.author.mention + " : " + msg)
+            await atr1.send(message.author.mention + "```css%s```관리자 : <@653172394499768322><@336425833403252746>"%msg)
+            await atr.send(message.author.mention + "```css%s```관리자 : <@653172394499768322><@336425833403252746>"%msg)
             await message.channel.send(message.author.mention + "```css\n정상적으로 문의를 보냈습니다.\n```")
 
     elif message.content.startswith("+관리자"):
@@ -74,6 +74,7 @@ async def on_message(message):
         await message.channel.send("?")
     elif message.content.startswith("김성은"):
         await message.channel.send("왜 불러")
+
 
 
 access_token = os.environ["BOT_TOKEN"]
